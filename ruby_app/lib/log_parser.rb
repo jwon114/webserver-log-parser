c3fcab69282f536f08560b834beef10fe8f8f06b
jwon114 <jthgwong@gmail.com>
2020-08-18 12:03:04 +0100

    parse file, development gems

--- ruby_app/lib/log_parser.rb
+++ ruby_app/lib/log_parser.rb
@@ -1,7 +1,27 @@
+require 'byebug'
+require 'error'
+
 class LogParser
-  attr_reader :file_path
-  
+  attr_reader :file_path, :visits
+
   def initialize(file_path)
-    @file_path = file_path
+    @file_path = file_exists?(file_path)
+    @visits = Hash.new
+  end
+
+  def parse
+    File.readlines(file_path).each do |line|
+      url, ip_address = *line.split(' ')
+      visits[url] ||= []
+      visits[url] << ip_address
+    end
+
+  end
+
+  private
+
+  def file_exists?(path)
+    raise FileNotFound.new('File does not exist') unless File.file?(path)
+    path
   end
 end
--- ruby_app/spec/log_parser_spec.rb
+++ ruby_app/spec/log_parser_spec.rb
@@ -1,12 +1,44 @@
 require './lib/log_parser'
 require 'spec_helper'
+require 'byebug'
 
 RSpec.describe LogParser do
-  let(:subject) { described_class.new('./data/webserver.log') }
+  let(:file_path) { './spec/data/webserver.log' }
+  let(:subject) { described_class.new(file_path) }
 
   describe '#initialize' do
     it 'loads the log file' do
-      expect(subject.file_path).to eq('./data/webserver.log')
+      expect(subject.file_path).to eq(file_path)
+    end
+
+    it 'sets visits to empty hash' do
+      expect(subject.visits).to eq(Hash.new)
+    end
+  end
+
+  describe '#parse' do
+    it 'opens the file and reads line by line' do
+      subject.parse
+      expected_visits = {
+        '/help_page/1' => ['126.318.035.038', '929.398.951.889', '722.247.931.582', '646.865.545.408'],
+        '/contact' => ['184.123.665.067'],
+        '/home'=> ['184.123.665.067', '235.313.352.950'],
+        '/about/2'=> ['444.701.448.104'],
+        '/index'=> ['444.701.448.104'],
+        '/about'=> ['061.945.150.735']
+      }
+      
+      expect(subject.visits).to eq(expected_visits)
+    end
+  end
+
+  describe '#file_exists?' do
+    it 'raises exception when file does not exist' do
+      expect { subject.send(:file_exists?, '') }.to raise_error(FileNotFound, 'File does not exist')
+    end
+
+    it 'returns file path if file exists' do
+      expect(subject.send(:file_exists?, file_path)).to eq(file_path)
     end
   end
 end
